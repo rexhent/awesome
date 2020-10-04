eb1877eef536eaa66eca9b8e2b103dc116a72d26
rexhent <dominichannah10@icloud.com>
2020-10-04 09:07:12 +0000

    1

--- rc.lua
+++ rc.lua
@@ -577,10 +577,19 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 --
 --
+--awful.spawn.with_shell(
+  --     'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
+    --   'xrdb -merge <<< "awesome.started:true";' ..
+       -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
+      -- 'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
+     --  )
+
 --
+beautiful.wallpaper = "/home/dhannah/.config/awesome/ttucker_taipei.jpg"
 beautiful.useless_gap = 8
 -- autostart
---
-awful.spawn.with_shell("nitrogen --restore")
+awful.spawn.with_shell("~/.config/awesome/wm.sh")
 awful.spawn.with_shell("picom &")
+
+--awful.spawn.with_shell("nitrogen --restore")
 
